d8f7859ff676a2854d683a2fa67a2442a03a4b79
juliapod23 <113717855+juliapod23@users.noreply.github.com>
2023-01-17 14:02:02 -0600

    commit
    commit

--- U7_ArrayLists/TicketLab/Ticketmaster.java
+++ U7_ArrayLists/TicketLab/Ticketmaster.java
@@ -6,11 +6,32 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Ticketmaster {
     private ArrayList<Show> shows;
+    private String filePath;
+
 
     //add constructor later
 
     //method to read in show data will populate each element of shows array
     //while loop of adding toStringed shows to ticketmaster toString, which will print the collection of toStringed shows
+
+
+    public void readShows() throws FileNotFoundException{
+        File myFile = new File(filePath);
+        Scanner fileIn = new Scanner(myFile);
+
+        //array of show objects
+        ArrayList<Show> showList = new ArrayList<Show>();
+        while(fileIn.hasNext()){
+            
+
+
+
+            Show temp = ;
+            shows.add(temp);
+        }
+
+
+    }
     public String toString(){
         String output = "Date" + "\t" + "Price" + "\t";
         return output;
--- U7_ArrayLists/TicketLab/TicketmasterDriver.java
+++ U7_ArrayLists/TicketLab/TicketmasterDriver.java
@@ -1,7 +1,21 @@
 package U7_ArrayLists.TicketLab;
 
+import java.util.Scanner;
+
 public class TicketmasterDriver {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        boolean tryAgain = true;
+        while(tryAgain){
+            try{
+                System.out.println("Please enter an int: ");
+                int num = in.nextInt();
+                System.out.println("You entered: " + num);
+                tryAgain = false;   //ends error checking while loop
+            } catch(Exception e){
+                System.out.println("Invalid entry");
+                in.nextLine();  //dummy read to clear bad data out
+            }
+        }   //will keep asking user for a num til a valid entry is entered
     }
 }
