a3782e54fa424b1e80a1c03ff43602f78b7bcf97
juliapod23 <113717855+juliapod23@users.noreply.github.com>
2023-02-24 13:41:45 -0600

    2/24/23

---
+++ U9_Inheritance/Homework/Athlete.java
@@ -0,0 +1,28 @@
+package U9_Inheritance.Homework;
+
+public class Athlete {
+    private String name;
+    private int age;
+
+    public Athlete(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
